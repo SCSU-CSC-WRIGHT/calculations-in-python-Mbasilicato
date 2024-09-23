2ca0418e778e527691114579e220963eb8a9a433
Mbasilicato <basilicatom1@southernct.edu>
2024-09-23 16:39:07 +0000

    comleted lab 1

---
+++ python_calculations.py
@@ -0,0 +1,7 @@
+p = int(input("What is the principal amount? "))
+
+print(p)
+r = int(input("What is the interest rate? "))
+t = int(input("What the time period? "))
+Simple_interest = (p*r*t)/(100)
+print("simple interest is ", Simple_interest)
