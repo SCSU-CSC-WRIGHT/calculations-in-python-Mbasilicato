8fcb2936e1fbe4e4f91278c752fd939075fc7da7
Mbasilicato <basilicatom1@southernct.edu>
2024-09-23 17:40:32 +0000

    completed Labs 1,2,and 3

--- python_calculations.py
+++ python_calculations.py
@@ -1,3 +1,6 @@
+
+#lab 1
+
 p = int(input("What is the principal amount? "))
 
 print(p)
@@ -5,3 +8,14 @@ r = int(input("What is the interest rate? "))
 t = int(input("What the time period? "))
 Simple_interest = (p*r*t)/(100)
 print("simple interest is ", Simple_interest)
+
+#Lab 2
+kg = float(input("what is your weight? "))
+m = float(input("what is your height?"))
+body_mass = (kg)/(m**2)
+print(body_mass)
+#Lab 3
+celsius = float(input("what is the temperature in celsius? "))
+fahrenhiet =(celsius * (9/5))+32
+print("the temperature in fahrenheit is ", fahrenhiet)
+print(f"the temp in f is {fahrenhiet}")
